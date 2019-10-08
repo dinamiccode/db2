c57fb1f8ef939f1632ecabe7eac7f67462a8e727
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2019-10-07 21:09:38 -0600

    Initial commit

---
+++ config/oracle.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'oracle' => [
+        'driver' => 'oracle',
+        'host' => 'LAPTOP-EMN1HV07',
+        'port' => '1521',
+        'database' => 'XE',
+        'service_name' => 'xe',
+        'username' => 'hr',
+        'password' => 'hr',
+        'charset' => '',
+        'prefix' => '',
+    ],
+];
+
+
+
